e73a4e2698e65d26f9ee4ad173573539c4bba750
Berlis <Berlisy2j@hotmail.com>
2022-07-22 16:33:00 -0400

    001 #comment maven project structure setup & some initials dependencies

---
+++ src/main/java/com/bersoft/bershopping/BerShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.bersoft.bershopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerShoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BerShoppingApplication.class, args);
+    }
+
+}
