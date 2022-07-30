8f5ac405564b5e2b482528e19d99f96a505ef821
Berlis <Berlisy2j@hotmail.com>
2022-07-30 19:05:58 -0400

    011 #comment change order of fields on itemDto class

--- src/main/java/com/bersoft/bershopping/utils/dtos/ItemDto.java
+++ src/main/java/com/bersoft/bershopping/utils/dtos/ItemDto.java
@@ -3,7 +3,8 @@ package com.bersoft.bershopping.utils.dtos;
 public record ItemDto(
         long productId,
         String productDesc,
+        double productStock,
         double price,
         double qty,
-        double productStock) {
+        double total) {
 }
