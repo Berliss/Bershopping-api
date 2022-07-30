99d6fe74f55e335bb6c4903fd17f04013e834e1b
Berlis <Berlisy2j@hotmail.com>
2022-07-30 19:05:20 -0400

    011 #comment add support into order mapper for payment method dto

--- src/main/java/com/bersoft/bershopping/utils/mappers/OrderMapper.java
+++ src/main/java/com/bersoft/bershopping/utils/mappers/OrderMapper.java
@@ -2,7 +2,9 @@ package com.bersoft.bershopping.utils.mappers;
 
 import com.bersoft.bershopping.persistence.entities.checkout.Order;
 import com.bersoft.bershopping.persistence.entities.customer.Customer;
+import com.bersoft.bershopping.persistence.entities.customer.PaymentMethod;
 import com.bersoft.bershopping.utils.dtos.OrderDto;
+import com.bersoft.bershopping.utils.dtos.PaymentMethodDto;
 
 import java.text.SimpleDateFormat;
 import java.util.LinkedHashMap;
@@ -12,7 +14,8 @@ public class OrderMapper {
     public OrderDto mapOrderToOrderDto(Order o) {
 
         Customer c = o.getCustomer();
-        Map<String,Object> customer = new LinkedHashMap<>();
+        PaymentMethod pm = o.getPaymentMethod();
+        Map<String, Object> customer = new LinkedHashMap<>();
 
         customer.put("id", c.getId());
         customer.put("firstName", c.getFirstName());
@@ -23,7 +26,7 @@ public class OrderMapper {
                 new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss").format(o.getCreateAt()),
                 o.getOrderItemList().size(),
                 o.getTotal(),
-                o.getPaymentMethod(),
+                new PaymentMethodDto(pm.getId(), pm.getPaymentType(), pm.getNumberReference()),
                 o.getDelivery(),
                 customer,
                 new ItemMapper().mapItemsToItemsDto(o.getOrderItemList())
