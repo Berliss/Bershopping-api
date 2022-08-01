fee537d0112d7bc046497dab4aaf19ac3694468d
Berlis <Berlisy2j@hotmail.com>
2022-08-01 17:32:55 -0400

    012 #comment fixing bugs found on testing, prevent return null

--- src/main/java/com/bersoft/bershopping/utils/ClaimsDecoder.java
+++ src/main/java/com/bersoft/bershopping/utils/ClaimsDecoder.java
@@ -2,12 +2,16 @@ package com.bersoft.bershopping.utils;
 
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
+import java.util.Optional;
+
 public class ClaimsDecoder {
 
     private ClaimsDecoder() {
     }
 
     public static String getEmailFromClaims(JwtAuthenticationToken principal) {
-        return principal.getToken().getClaims().get("email").toString();
+      return   Optional.ofNullable(principal)
+                .map(token -> token.getToken().getClaims().get("email").toString())
+                .orElse("");
     }
 }
