227e304cecd5234a7c8106fc4ee6ee309de935ca
Berlis <Berlisy2j@hotmail.com>
2022-08-01 17:37:09 -0400

    012 #comment fixing bugs found on testing, custom messages added

--- src/main/java/com/bersoft/bershopping/utils/dtos/ProductToAddDto.java
+++ src/main/java/com/bersoft/bershopping/utils/dtos/ProductToAddDto.java
@@ -2,4 +2,4 @@ package com.bersoft.bershopping.utils.dtos;
 
 import javax.validation.constraints.NotNull;
 
-public record ProductToAddDto( @NotNull Long id, @NotNull Double quantity) {}
+public record ProductToAddDto( @NotNull(message = "id must be a positive numeric value") Long id, @NotNull(message = "quantity must be a positive numeric value") Double quantity) {}
